0b3082e2ff84cb30436676c9bdc8f3bc82b690df
rafaeljana <Rafy176@gmail.com>
2025-03-24 11:40:55 +0200

    Rafael.java print change

--- RafaelJana.java
+++ RafaelJana.java
@@ -2,7 +2,7 @@ public class RafaelJana{
 
 	public static void main(String[] args){
 		for (int i =1; i<=10;i++){
-			System.out.println("Rafael Jana");
+			System.out.println("Rafael Jana - Different print Test");
 		}
 	}
 }
